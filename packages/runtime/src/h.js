cf4b59377670b7ba5ec44fd6a0e7822abf406060
Angelo Faella <revangel@outlook.it>
2024-02-06 16:11:30 +0100

    add patching algorithm

--- packages/runtime/src/h.js
+++ packages/runtime/src/h.js
@@ -31,3 +31,20 @@ export function h(tag, props = {}, children = []) {
     type: DOM_TYPES.ELEMENT,
   };
 }
+
+export function extractChildren(vdom) {
+  if (vdom.children == null) {
+    return [];
+  }
+
+  const children = [];
+  for (const child of vdom.children) {
+    if (child.type === DOM_TYPES.FRAGMENT) {
+      children.push(...extractChildren(child, children));
+    } else {
+      children.push(child);
+    }
+  }
+
+  return children;
+}
